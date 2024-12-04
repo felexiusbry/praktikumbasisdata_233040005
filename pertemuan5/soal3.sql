283e693ab535f9bf86574bc88e917b6a70f1af36
felexiusbry <147691391+felexiusbry@users.noreply.github.com>
2024-12-04 14:19:42 +0700

    Add files via upload

---
+++ pertemuan5/soal3.sql
@@ -0,0 +1,3 @@
+use pertemuan_5;
+ALTER TABLE jurusan
+ALTER COLUMN nama_jurusan VARCHAR(100) not null;
---
+++ pertemuan5/soal4.sql
@@ -0,0 +1,9 @@
+INSERT INTO jurusan (id,nama_jurusan)
+VALUES
+(1,'Teknologi Pangan');
+(2,'Teknik Metalurgi');
+(3,'Teknik Geologi');
+(4,'Teknik Komputer');
+(5,'Teknik Kimia');
+
+select * from jurusan
